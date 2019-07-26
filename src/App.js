9247c10f42fdaa8b8ed69a757cf3d6bcedc8738a
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-26 09:22:31 -0400

    Add Header, Die, Hand, Controls

--- src/App.js
+++ src/App.js
@@ -8,12 +8,19 @@ const newHand = (numDice = 6) =>
     .fill(null)
     .map(() => randomDie());
 
-const Die = styled.div`
+const Header = () => {
+  return (
+    <header>
+      <h1>Farkle</h1>
+    </header>
+  );
+};
+
+const DieStyles = styled.div`
   display: inline-flex;
   justify-content: center;
   align-items: center;
-  background: black;
-  color: white;
+  background: #ccc;
   border-radius: 0.5rem;
   width: 3rem;
   height: 3rem;
@@ -21,21 +28,42 @@ const Die = styled.div`
   margin: 0.5rem;
 `;
 
+const Die = ({ value, handleClick }) => {
+  return <DieStyles onClick={handleClick}>{value}</DieStyles>;
+};
+
+const Hand = ({ dice }) => {
+  return (
+    <div>
+      <h3>Hand</h3>
+      {dice.map(d => (
+        <Die value={d} />
+      ))}
+    </div>
+  );
+};
+
+const Controls = ({ callbacks }) => {
+  return (
+    <div>
+      <button onClick={callbacks.rollDice}>Roll</button>
+    </div>
+  );
+};
+
 function App() {
-  const [hand, setHand] = useState(newHand());
+  const [diceInHand, setDiceInHand] = useState(newHand());
+
+  const rollDice = () => {
+    setDiceInHand(prev => newHand(prev.length));
+  };
 
   return (
     <div>
-      <header>
-        <h1>Farkle</h1>
-      </header>
+      <Header />
       <main>
-        In hand:{' '}
-        {hand.map(d => (
-          <Die key={d} value={d}>
-            {d}
-          </Die>
-        ))}
+        <Hand dice={diceInHand} />
+        <Controls callbacks={{ rollDice }} />
       </main>
     </div>
   );
