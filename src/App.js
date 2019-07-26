d4d92b64aad702c26f58c6a1856420dad82d19a7
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-26 08:59:16 -0400

    Render dice hand

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
 const randomDie = () => Math.ceil(Math.random() * 6);
 
 const newHand = () =>
@@ -5,12 +8,35 @@ const newHand = () =>
     .fill(null)
     .map(() => randomDie());
 
+const Die = styled.div`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  background: black;
+  color: white;
+  border-radius: 0.5rem;
+  width: 3rem;
+  height: 3rem;
+  font-size: 2em;
+  margin: 0.5rem;
+`;
+
 function App() {
+  const [hand, setHand] = useState(newHand());
+
   return (
     <div>
       <header>
         <h1>Farkle</h1>
       </header>
+      <main>
+        In hand:{' '}
+        {hand.map(d => (
+          <Die key={d} value={d}>
+            {d}
+          </Die>
+        ))}
+      </main>
     </div>
   );
 }
