2957a40ef78e5cfc4484f45c3579731d9b5767a2
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-26 12:12:34 -0400

    Fiddle with styles

--- src/components/Die.js
+++ src/components/Die.js
@@ -5,11 +5,12 @@ const DieStyles = styled.div`
   display: inline-flex;
   justify-content: center;
   align-items: center;
+  position: relative;
   background: #ccc;
   border-radius: 0.5rem;
   width: 3rem;
   height: 3rem;
-  font-size: 2em;
+  font-size: 1.5em;
   margin: 0.5rem;
 `;
 
