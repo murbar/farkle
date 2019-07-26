2d55c0db96a257b0bdf688ef6000a6f5d1c8b02d
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-26 17:13:40 -0400

    Add pips

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
+import { ReactComponent as PipsSprite } from '../images/pips.svg';
 
 const DieStyles = styled.div`
   display: inline-flex;
   justify-content: center;
   align-items: center;
   position: relative;
-  background: #ccc;
-  border-radius: 0.5rem;
+  border-radius: 0.35rem;
   width: 3rem;
   height: 3rem;
   font-size: 1.5em;
   margin: 0.5rem;
+  background: #eee;
+  border: 1px solid #ccc;
+  overflow: hidden;
+`;
+
+const Pips = styled(PipsSprite)`
+  position: absolute;
+  width: 600%;
+  height: 100%;
+  top: 0;
+  left: ${p => `-${(p.count - 1) * 100}%`};
+  color: #666;
 `;
 
 export default function Die({ value, handleClick }) {
-  return <DieStyles onClick={() => handleClick(value)}>{value}</DieStyles>;
+  return (
+    <DieStyles onClick={() => handleClick(value)}>
+      <Pips count={value} />
+    </DieStyles>
+  );
 }
